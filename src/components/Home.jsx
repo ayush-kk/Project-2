5ecc99cfffb3c7ccc0cdc4c4eaa548305844a4e6
ayushkumar37 <ayushkumar.official37@gmail.com>
2023-04-09 16:24:53 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,115 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Sam Kumar is an accomplished Full Stack Developer with an extensive
+            skill set in building dynamic and interactive web applications. He
+            has a solid foundation in React JS, Springboot, JPA, Hibernate,
+            Postgres, Lombok, Swagger, and Postman. His dedication to his craft
+            and passion for programming has led him to develop robust and
+            scalable solutions that exceed client expectations. <br />
+            Sam completed his B.Tech in Electronics and Communication from
+            Jaipur Engineering College and Research Center, where he gained a
+            strong understanding of the core principles of engineering. His
+            education has provided him with a solid foundation in
+            problem-solving and critical thinking, which he applies to his work
+            as a developer.
+            <br /> Sam is a highly motivated and detail-oriented professional
+            who strives to create efficient, high-quality code. His expertise in
+            various programming languages and frameworks, such as React, HTML,
+            CSS, JS, Redux, React Router DOM, and Axios, makes him an asset to
+            any project. With a keen eye for design and user experience, Sam has
+            delivered exceptional solutions that have won the admiration of
+            clients and users alike.
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
